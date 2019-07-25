9feef5a4b5d6d735ec2a99b38ebd15190814ac5d
Atul Bahniwal <atulbahniwal@Atuls-MacBook-Air.local>
2019-07-25 21:51:58 +0530

    recipe finder

---
+++ src/components/Recipe.js
@@ -0,0 +1,54 @@
+import React, { Component } from "react";
+
+export default class Recipe extends Component {
+  render() {
+    const {
+      image_url,
+      title,
+      source_url,
+      publisher,
+      recipe_id
+    } = this.props.recipe;
+
+    const { handleDetails } = this.props;
+
+    return (
+      <div className="col-10 mx-auto col-md-6 col-lg-4 my-3">
+        <div className="card">
+          <img
+            src={image_url}
+            className="img-card-top"
+            style={{ height: "14rem" }}
+            alt="recipe"
+          />
+          <div className="card-body text-capitalize">
+            <h6>
+              {title.length < 36 ? title : `${title.substring(0, 35)}...`}
+            </h6>
+
+            <h6 className="text-warning text-slanted">
+              provided by {publisher}
+            </h6>
+          </div>
+          <div className="card-footer">
+            <button
+              onClick={handleDetails}
+              type="button"
+              className="btn btn-primary text-capitalize"
+            >
+              Details
+            </button>
+            <a
+              href={source_url}
+              className="btn btn-success mx-2 text-capitalize"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Recipe url
+            </a>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
